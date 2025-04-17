b46aba37b3bdc8f587a01750431aa5ac0324645f
freddyfazbeareauheauh <szteroidman@gmail.com>
2025-04-17 18:21:26 +0200

    corsfilter megint

--- mozixx/src/main/java/filters/CORSFilter.java
+++ mozixx/src/main/java/filters/CORSFilter.java
@@ -5,24 +5,42 @@
 package filters;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CORSFilter implements ContainerResponseFilter{
+public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        if (isPreflightRequest(requestContext)) {
+            requestContext.abortWith(Response.ok().build());
+        }
+    }
+
+    private boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null
+                && request.getMethod().equalsIgnoreCase("OPTIONS");
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext, 
-                      ContainerResponseContext responseContext) {
+                     ContainerResponseContext responseContext) {
         responseContext.getHeaders().add(
             "Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add(
             "Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add(
             "Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization");
+            "origin, content-type, accept, authorization, x-requested-with");
         responseContext.getHeaders().add(
-            "Access-Control-Allow-Methods", 
+            "Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add(
+            "Access-Control-Expose-Headers",
+            "content-type, authorization");
     }
 }
